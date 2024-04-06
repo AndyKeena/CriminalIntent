aafe2efa1d67fdc79949411309d8c3bc5bbf0d99
andrea <okeena.elders@gmail.com>
2024-04-06 18:24:17 +0530

    Task 2

--- app/src/main/java/uk/ac/wlv/criminalintent/CrimeListFragment.java
+++ app/src/main/java/uk/ac/wlv/criminalintent/CrimeListFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -31,7 +32,7 @@ public class CrimeListFragment extends Fragment{
         mAdapter = new CrimeAdapter(crimes);
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
-    private class CrimeHolder extends RecyclerView.ViewHolder{
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private TextView mTitleTextView;
         private TextView mDateTextView;
         private CheckBox  mSolvedCheckBox;
@@ -42,6 +43,8 @@ public class CrimeListFragment extends Fragment{
             mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_crime_title_text_view);
             mDateTextView = (TextView) itemView.findViewById(R.id.list_item_crime_date_text_view);
             mSolvedCheckBox = (CheckBox) itemView.findViewById(R.id.list_item_crime_solved_check_box);
+
+            itemView.setOnClickListener(this);
         }
 
         public void bindCrime(Crime crime){
@@ -49,6 +52,11 @@ public class CrimeListFragment extends Fragment{
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
             mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+
+        @Override
+        public void onClick(View v){
+            Toast.makeText(getActivity(), "Clicked" + mTitleTextView.getText(), Toast.LENGTH_SHORT).show();
         }
     }
 
